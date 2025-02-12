14bb88670733f02dd11ad632f303d293517e4ccb
Louis <Louismmaduabuchi1@gmail.com>
2025-02-12 09:45:35 +0000

    Terraform infrastructure deployment to azure cloud

--- 5_aks.tf
+++ 5_aks.tf
@@ -8,7 +8,7 @@ resource "azurerm_user_assigned_identity" "base" {
 }
 
 resource "azurerm_role_assignment" "base" {
-  scope                = azurerm_resource_group.this.id
+  scope                = azurerm_resource_group.this.name
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_user_assigned_identity.base.principal_id
 }
